0867a23f901144d39afd9ba169b506412a8a86e0
Carlos Rueda <carueda@mbari.org>
2020-05-30 15:19:28 -0700

    use a q-table to show the ontology list

--- setup/README.md
+++ setup/README.md
@@ -17,3 +17,9 @@ A second commit with configuring axios to access the COR ORR API, getting the li
 and displaying that list in the main index page, initially just displaying the JSON directly:
 
 ![](ontlistjson.png)
+
+----
+
+Using a q-table to show the ontology list:
+
+![](ontlisttable.png)
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,7 +1,17 @@
 <template>
   <q-page>
-    <div v-if="loading">Loading ...</div>
-    <pre v-else>{{ ontologies }}</pre>
+    <q-table
+      :loading="loading"
+      title="Ontologies"
+      :data="ontologies"
+      :columns="columns"
+      row-key="uri"
+      :pagination.sync="pagination"
+      :dense="$q.screen.lt.md"
+      :grid="$q.screen.lt.md"
+      flat
+      bordered
+    />
   </q-page>
 </template>
 
@@ -13,6 +23,70 @@
       return {
         loading: false,
         ontologies: [],
+
+        columns: [
+          {
+            name: 'uri',
+            label: 'IRI',
+            align: 'left',
+            field: row => row.uri,
+            format: val => `${val}`,
+            sortable: true
+          },
+          {
+            name: 'name',
+            label: 'Name',
+            align: 'left',
+            field: row => row.name,
+            format: val => `${val}`,
+            sortable: true
+          },
+          {
+            name: 'ownerName',
+            label: 'Owner',
+            align: 'left',
+            field: row => row.ownerName,
+            format: val => `${val}`,
+            sortable: true
+          },
+          {
+            name: 'author',
+            label: 'Author',
+            align: 'left',
+            field: row => row.author,
+            format: val => `${val}`,
+            sortable: true
+          },
+          {
+            name: 'status',
+            label: 'Status',
+            align: 'left',
+            field: row => row.status,
+            format: val => `${val}`,
+            sortable: true
+          },
+          {
+            name: 'visibility',
+            label: 'Visibility',
+            align: 'left',
+            field: row => row.visibility,
+            format: val => `${val}`,
+            sortable: true
+          },
+          {
+            name: 'version',
+            label: 'Version',
+            align: 'left',
+            field: row => row.version,
+            format: val => `${val}`,
+            sortable: true
+          },
+        ],
+        pagination: {
+          sortBy: 'version',
+          descending: true,
+          rowsPerPage: 50,
+        },
       }
     },
 
