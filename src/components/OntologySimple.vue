4910af7ead3270cb26f5d80979568cbdd2fce528
Carlos Rueda <carueda@mbari.org>
2020-09-18 14:40:04 -0700

    escape #

--- src/components/OntologySimple.vue
+++ src/components/OntologySimple.vue
@@ -1,61 +1,62 @@
 <template>
   <q-markup-table
-    flat dense
+    flat
+    dense
     separator="none"
   >
     <tbody>
-    <tr>
-      <th style="text-align:right">IRI:</th>
-      <td>
-        <router-link
-          style="text-decoration:none"
-          :to="`/ont/${ontology.uri}`"
-        >
-          {{ ontology.uri }}
-        </router-link>
-      </td>
-    </tr>
-    <tr>
-      <th style="text-align:right">Name:</th>
-      <td>{{ ontology.name }}</td>
-    </tr>
-    <tr>
-      <th style="text-align:right">Version:</th>
-      <td>{{ ontology.version }}</td>
-    </tr>
-    <tr>
-      <th style="text-align:right">Author:</th>
-      <td>{{ ontology.author }}</td>
-    </tr>
-    <tr>
-      <th style="text-align:right">Owner:</th>
-      <td>{{ ontology.ownerName }}</td>
-    </tr>
-    <tr>
-      <th style="text-align:right">Status:</th>
-      <td>{{ ontology.status }}</td>
-    </tr>
-    <tr>
-      <th style="text-align:right">Visibility:</th>
-      <td>{{ ontology.visibility }}</td>
-    </tr>
-    <tr>
-      <th style="text-align:right">Format:</th>
-      <td>{{ ontology.format }}</td>
-    </tr>
+      <tr>
+        <th style="text-align:right">IRI:</th>
+        <td>
+          <router-link
+            style="text-decoration:none"
+            :to="`/ont/${ontology.uri.replace('#', '%23')}`"
+          >
+            {{ ontology.uri }}
+          </router-link>
+        </td>
+      </tr>
+      <tr>
+        <th style="text-align:right">Name:</th>
+        <td>{{ ontology.name }}</td>
+      </tr>
+      <tr>
+        <th style="text-align:right">Version:</th>
+        <td>{{ ontology.version }}</td>
+      </tr>
+      <tr>
+        <th style="text-align:right">Author:</th>
+        <td>{{ ontology.author }}</td>
+      </tr>
+      <tr>
+        <th style="text-align:right">Owner:</th>
+        <td>{{ ontology.ownerName }}</td>
+      </tr>
+      <tr>
+        <th style="text-align:right">Status:</th>
+        <td>{{ ontology.status }}</td>
+      </tr>
+      <tr>
+        <th style="text-align:right">Visibility:</th>
+        <td>{{ ontology.visibility }}</td>
+      </tr>
+      <tr>
+        <th style="text-align:right">Format:</th>
+        <td>{{ ontology.format }}</td>
+      </tr>
     </tbody>
   </q-markup-table>
 </template>
 
 <script>
-  export default {
-    name: 'ont-simple',
+export default {
+  name: 'ont-simple',
 
-    props: {
-      ontology: {
-        type: Object,
-        required: true
-      },
+  props: {
+    ontology: {
+      type: Object,
+      required: true
     },
-  }
+  },
+}
 </script>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -14,36 +14,57 @@
     >
       <template v-slot:body="props">
         <q-tr :props="props">
-          <q-td key="uri" :props="props">
+          <q-td
+            key="uri"
+            :props="props"
+          >
             <router-link
               style="text-decoration:none"
-              :to="`/ont/${props.row.uri}`"
+              :to="`/ont/${props.row.uri.replace('#', '%23')}`"
             >
               {{ props.row.uri }}
             </router-link>
           </q-td>
 
-          <q-td key="name" :props="props">
+          <q-td
+            key="name"
+            :props="props"
+          >
             {{ props.row.name }}
           </q-td>
 
-          <q-td key="ownerName" :props="props">
+          <q-td
+            key="ownerName"
+            :props="props"
+          >
             {{ props.row.ownerName }}
           </q-td>
 
-          <q-td key="author" :props="props">
+          <q-td
+            key="author"
+            :props="props"
+          >
             {{ props.row.author }}
           </q-td>
 
-          <q-td key="status" :props="props">
+          <q-td
+            key="status"
+            :props="props"
+          >
             {{ props.row.status }}
           </q-td>
 
-          <q-td key="visibility" :props="props">
+          <q-td
+            key="visibility"
+            :props="props"
+          >
             {{ props.row.visibility }}
           </q-td>
 
-          <q-td key="version" :props="props">
+          <q-td
+            key="version"
+            :props="props"
+          >
             {{ props.row.version }}
           </q-td>
         </q-tr>
@@ -51,9 +72,7 @@
 
       <template v-slot:item="props">
         <q-card class="q-ma-sm">
-          <ontology-simple
-            :ontology="props.row"
-          />
+          <ontology-simple :ontology="props.row" />
         </q-card>
       </template>
 
@@ -62,106 +81,106 @@
 </template>
 
 <script>
-  import OntologySimple from 'components/OntologySimple'
+import OntologySimple from 'components/OntologySimple'
 
-  export default {
-    name: 'PageIndex',
+export default {
+  name: 'PageIndex',
 
-    components: {
-      OntologySimple,
-    },
+  components: {
+    OntologySimple,
+  },
 
-    data() {
-      return {
-        loading: false,
-        ontologies: [],
+  data () {
+    return {
+      loading: false,
+      ontologies: [],
 
-        columns: [
-          {
-            name: 'uri',
-            label: 'IRI',
-            align: 'left',
-            field: row => row.uri,
-            format: val => `${val}`,
-            sortable: true
-          },
-          {
-            name: 'name',
-            label: 'Name',
-            align: 'left',
-            field: row => row.name,
-            format: val => `${val}`,
-            sortable: true
-          },
-          {
-            name: 'ownerName',
-            label: 'Owner',
-            align: 'left',
-            field: row => row.ownerName,
-            format: val => `${val}`,
-            sortable: true
-          },
-          {
-            name: 'author',
-            label: 'Author',
-            align: 'left',
-            field: row => row.author,
-            format: val => `${val}`,
-            sortable: true
-          },
-          {
-            name: 'status',
-            label: 'Status',
-            align: 'left',
-            field: row => row.status,
-            format: val => `${val}`,
-            sortable: true
-          },
-          {
-            name: 'visibility',
-            label: 'Visibility',
-            align: 'left',
-            field: row => row.visibility,
-            format: val => `${val}`,
-            sortable: true
-          },
-          {
-            name: 'version',
-            label: 'Version',
-            align: 'left',
-            field: row => row.version,
-            format: val => `${val}`,
-            sortable: true
-          },
-        ],
-        pagination: {
-          sortBy: 'version',
-          descending: true,
-          rowsPerPage: 50,
+      columns: [
+        {
+          name: 'uri',
+          label: 'IRI',
+          align: 'left',
+          field: row => row.uri,
+          format: val => `${val}`,
+          sortable: true
         },
+        {
+          name: 'name',
+          label: 'Name',
+          align: 'left',
+          field: row => row.name,
+          format: val => `${val}`,
+          sortable: true
+        },
+        {
+          name: 'ownerName',
+          label: 'Owner',
+          align: 'left',
+          field: row => row.ownerName,
+          format: val => `${val}`,
+          sortable: true
+        },
+        {
+          name: 'author',
+          label: 'Author',
+          align: 'left',
+          field: row => row.author,
+          format: val => `${val}`,
+          sortable: true
+        },
+        {
+          name: 'status',
+          label: 'Status',
+          align: 'left',
+          field: row => row.status,
+          format: val => `${val}`,
+          sortable: true
+        },
+        {
+          name: 'visibility',
+          label: 'Visibility',
+          align: 'left',
+          field: row => row.visibility,
+          format: val => `${val}`,
+          sortable: true
+        },
+        {
+          name: 'version',
+          label: 'Version',
+          align: 'left',
+          field: row => row.version,
+          format: val => `${val}`,
+          sortable: true
+        },
+      ],
+      pagination: {
+        sortBy: 'version',
+        descending: true,
+        rowsPerPage: 50,
+      },
+    }
+  },
+
+  mounted () {
+    this.getOntologies()
+  },
+
+  methods: {
+    async getOntologies () {
+      this.loading = true
+      const method = 'GET'
+      const url = '/ont'
+      let params = {}
+      try {
+        const result = await this.$axios({ method, url, params })
+        this.loading = false
+        this.ontologies = result.data
       }
-    },
-
-    mounted() {
-      this.getOntologies()
-    },
-
-    methods: {
-      async getOntologies() {
-        this.loading = true
-        const method = 'GET'
-        const url = '/ont'
-        let params = {}
-        try {
-          const result = await this.$axios({method, url, params})
-          this.loading = false
-          this.ontologies = result.data
-        }
-        catch (error) {
-          this.loading = false
-          console.error('getOntologies', error)
-        }
+      catch (error) {
+        this.loading = false
+        console.error('getOntologies', error)
       }
     }
   }
+}
 </script>
